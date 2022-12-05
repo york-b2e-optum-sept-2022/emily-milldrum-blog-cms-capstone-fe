c49ed4cc56d00ed2724c3db6b4fd9a2e4852839b
Emily <emilldr>
2022-12-05 13:37:00 -0600

    more setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { RegisterComponent } from './register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {FormsModule} from "@angular/forms";
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {AccountService} from "../_services/account.service";
+import {MainService} from "../_services/main.service";
+import {STATE} from "../_enums/STATE";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  errorMessage: any;
 
+
+  constructor(private service: AccountService, private mainService: MainService) {
+  }
+  cancelRegClick() {
+    this.mainService.$state.next(STATE.login)
+  }
+
+  onRegisterClick(regForm: NgForm) {
+    console.log(regForm)
+
+  }
 }
